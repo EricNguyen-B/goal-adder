4f16b89af828bb250a8482e089a6df762e76e199
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-12-13 09:30:18 -0500

    simplifying into multiple components

--- App.js
+++ App.js
@@ -8,41 +8,23 @@ import {
   ScrollView,
   FlatList } from 'react-native';
 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  }; 
-
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [...currentGoals, 
-      { key: Math.random().toString(), text : enteredGoal}]);
+      { key: Math.random().toString(), text : goalTitle}]);
   };
 
   return (
     <View style={styles.screen}>
-
-      <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Monthly Goals" 
-          style={styles.input} 
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-          />
-
-        {/* Add another goal */}
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList 
         data={courseGoals} 
-        renderItem={itemData => (
-        <View style={styles.listItem}>
-            <Text>{itemData.item.text}</Text>
-          </View>
-        )}
+        renderItem={itemData => <GoalItem title={itemData.item.text}/>}
       />
     </View>
   );
@@ -52,22 +34,4 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection:'row',
-    justifyContent : 'space-between', 
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
 });
